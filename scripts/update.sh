ceac2c8e887706b971131070864a0b20bfc19f0b
jerryyf <jerryyf@linsomnia.net>
2024-05-08 02:52:20 +1000

    Add configs + cleanup directory

---
+++ scripts/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# util to update configs to my git directory
+
+cp -r ~/.config/hypr ./.config/
+cp -r ~/.config/nvim ./.config/
+cp -r ~/.config/waybar ./.config/
+cp -r ~/.config/kitty ./.config/
+cp -r ~/.zshrc .
+
+exit 0
