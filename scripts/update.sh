557c1fd57555a938e6a3ae10cbab379f20b3a08f
jerryyf <jerryyf@linsomnia.net>
2024-05-19 16:44:20 +1000

    Fix update script copying to wrong directory

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,10 +2,10 @@
 
 # util to update configs to my git directory
 
-cp -r ~/.config/hypr ./.config/
-cp -r ~/.config/nvim ./.config/
-cp -r ~/.config/waybar ./.config/
-cp -r ~/.config/kitty ./.config/
-cp -r ~/.zshrc .
+cp -r ~/.config/hypr ../.config/
+cp -r ~/.config/nvim ../.config/
+cp -r ~/.config/waybar ../.config/
+cp -r ~/.config/kitty ../.config/
+cp -r ~/.zshrc ..
 
 exit 0
